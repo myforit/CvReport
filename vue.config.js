783b9b67af404af87411443d5f64e2681fddc17b
songjj <songjj@zetyun.com>
2022-12-05 09:59:49 +0800

    fix:proxy

--- vue.config.js
+++ vue.config.js
@@ -3,19 +3,17 @@ module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
   publicPath: './',
-  configureWebpack: {
-    devServer: {
-      port: 8080,
-      proxy: {
-        //跨域代理
-        '/dataset': {
-          target: 'http://172.20.1.181:9900/metacv/data/query',
-          ws: true,
-          secure: false,
-          changeOrigin: true, //是否开启跨域
-          pathRewrite: {
-            '^/dataset': '', //让路径以/api开头的字段为空
-          },
+  devServer: {
+    port: 8080,
+    proxy: {
+      //跨域代理
+      '/dataset': {
+        target: 'http://172.20.1.181:9900/metacv/data/query',
+        ws: true,
+        secure: false,
+        changeOrigin: true, //是否开启跨域
+        pathRewrite: {
+          '^/dataset': '', //让路径以/api开头的字段为空
         },
       },
     },
