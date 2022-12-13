a0d3737e68193db07bb2587966b62398d019477d
songjj <songjj@zetyun.com>
2022-12-13 13:32:37 +0800

    add:deploy

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
-  publicPath: './',
+  publicPath: process.env.NODE_ENV === 'production' ? '/CvReport/' : '/',
   devServer: {
     proxy: {
       //跨域代理
